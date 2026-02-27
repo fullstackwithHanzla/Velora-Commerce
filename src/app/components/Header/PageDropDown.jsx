15d0fc7aac4f81554b5117f2a1df09a4db4b2e8c
Hanzla <fullstackwithhanzla@gmail.com>
2026-02-27 20:08:25 +0500

    dropdown glitch fixed

--- src/app/components/Header/PageDropDown.jsx
+++ src/app/components/Header/PageDropDown.jsx
@@ -13,14 +13,14 @@ const PageDropDown = () => {
   return (
     <div
       className={`
-        absolute left-1/2 top-full mt-2 -translate-x-1/2
+        absolute left-1/2 top-full  -translate-x-1/2
         transition-all duration-200 ease-out
         ${isPageDropdownOpen
           ? 'opacity-100 translate-y-0 pointer-events-auto'
           : 'opacity-0 translate-y-2 pointer-events-none'}
       `}
     >
-      <div className="bg-white shadow-md rounded-sm min-w-60">
+      <div className="bg-white py-2 shadow-md rounded-sm min-w-60">
         <nav className="flex flex-col px-3 my-2 gap-2">
           <PagesNavLink />
         </nav>
